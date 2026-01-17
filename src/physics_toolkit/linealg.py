f471d2ea401eeff4b4cc235bd320c0e322050cf8
francoislang <langfrancoisfrederic@gmail.com>
2026-01-18 00:32:07 +0100

    wip: add eigh_sorted

--- src/physics_toolkit/linealg.py
+++ src/physics_toolkit/linealg.py
@@ -4,3 +4,10 @@ from numpy.typing import NDArray
 
 def is_hermitian(matrix: NDArray[np.complexfloating | np.floating]) -> bool:
     return np.allclose(matrix, matrix.conj().T)
+
+def eigh_sorted(matrix: NDArray[np.complexfloating | np.floating]) -> tuple[NDArray[np.floating], NDArray[np.complexfloating]]:
+    if not is_hermitian(matrix):
+        raise ValueError("Matrix is not Hermitian")
+    eigvalues, eigvecs = np.linalg.eigh(matrix)
+    sort_indices = np.argsort(eigvalues)
+    return eigvalues[sort_indices], eigvecs[:, sort_indices]
